7c7bef0f166e330d124843f6b483f77f397763dd
Lionel Martin <lm@purebilling.com>
2018-02-26 10:42:52 +0100

    Add data extraction sample in IPN php example

--- www/ipn.php
+++ www/ipn.php
@@ -29,7 +29,7 @@ $client = new LyraNetwork\Client();
 
 /* No POST data ? paid page in not called after a payment form */
 if (empty($_POST)) {
-    throw new Exception("no post data received!");
+    throw new Exception('no post data received!');
 }
 
 /* Check the signature using password */
@@ -38,14 +38,19 @@ if (!$client->checkHash()) {
     //something wrong, probably a fraud ....
     signature_error($formAnswer['kr-answer']['transactions'][0]['uuid'], $hashKey, 
                     $client->getLastCalculatedHash(), $_POST['kr-hash']);
-    throw new Exception("invalid signature");
+    throw new Exception('invalid signature');
 }
 
 /* Retrieve the IPN content */
 $formAnswer = $client->getParsedFormAnswer();
 
 /* Retrieve the billingTransaction id from the IPN data */
-$transaction = $formAnswer["transactions"][0];
+$answer = $formAnswer['transactions'][0];
+
+/* get some parameters from the answer */
+$orderStatus = $answer = $formAnswer['orderStatus'];
+$orderId = $answer = $formAnswer['orderDetails']['orderId'];
+$transactionUuid = $formAnswer['orderDetails'][0]['uuid'];
 
 /* I update my database if needed */
 /* Add here your custom code */ 
@@ -55,5 +60,5 @@ $transaction = $formAnswer["transactions"][0];
  * You can return want you want but
  * HTTP response code should be 200
  */
-print "OK!";
+print 'OK!';
 ?>
