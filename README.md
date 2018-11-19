6fd51b19a125e765cd8d1620fe37657a455d32a3
Lionel Martin <lm@purebilling.com>
2018-11-19 11:49:13 +0100

    Add heroku example, fix createToken.php

--- README.md
+++ README.md
@@ -27,3 +27,29 @@ and go to http://localhost:6980
 ## Try it using your favorite web-server
 
 Copy src/ directory content to your PHP server, and go to *index.html* page.
+
+## Try on on Heroku
+
+You can also simply install the php examples on Heroku for free. First you need
+
+- a valid account, see https://www.heroku.com
+- keroky cli installed: https://devcenter.heroku.com/articles/getting-started-with-php#set-up
+
+Next, to deploy php examples, do:
+
+- cd www
+- heroku create
+- git subtree push --prefix www heroku master
+
+Start the container and open it in your web-brower
+
+- heroku ps:scale web=1
+- heroku open
+
+To publish updated code, do:
+
+- git commit -am"some updates"
+- git push heroku master
+- heroku open
+
+and see yout changes on the web-brower.
--- www/createToken.php
+++ www/createToken.php
@@ -7,35 +7,38 @@ require_once __DIR__ . '/keys.php';
 
 /** 
  * Initialize the SDK 
- * Please update your keys in keys.php
+ * see keys.php
  */
-$client = new Lyra\Client();         /* Create the client SDK */
-$client->setUsername($_username);           /* username defined in keys.php file */
-$client->setPassword($_password);           /* password defined in keys.php file */
-$client->setPublicKey($_publicKey);         /* key defined in keys.php file */
-$client->setEndpoint($_endpoint);           /* REST API endpoint defined in keys.php file */
+$client = new Lyra\Client();
 
 if (isset($_GET['requestObject'])) {
-    /**
-     * I create a formToken
-     */
     $store = json_decode($_GET['requestObject']);
-    $response = $client->post("V4/Charge/CreatePayment", $store);
-
-    /* I check if there is some errors */
-    if ($response['status'] != 'SUCCESS') {
-        /* an error occurs, I throw an exception */
-        $error = $response['answer'];
-        header("Status: 500", true, 500);
-        echo "error " . $error['errorCode'] . ": " . $error['errorMessage'];
-        die();
-    }
-
-    /* everything is fine, I extract the formToken */
-    $formToken = $response["answer"]["formToken"];
-    header("Content-Type", "application/json");
-    echo $formToken;
 } else {
-    header("Status: 500", true, 500);
-    echo "Invalid input data";
+    $store = array( "amount" => 250, 
+                    "currency" => "EUR", 
+                    "customer" => array(
+                    "email" => "sample@example.com",
+                    "orderId" => uniqid("MyOrderId")
+                    ));
 }
+
+/**
+ * I create a formToken
+ */
+
+$response = $client->post("V4/Charge/CreatePayment", $store);
+
+//* I check if there is some errors */
+if ($response['status'] != 'SUCCESS') {
+    /* an error occurs */
+    $error = $response['answer'];
+    header("Content-Type", "application/json");
+    header('HTTP/1.1 500 Internal Server Error');
+    echo '{"error": "' . $error['errorCode'] . '", "_type": "DemoError" }';
+    die();
+}
+
+/* everything is fine, I extract the formToken */
+$formToken = $response["answer"]["formToken"];
+header("Content-Type", "application/json");
+echo '{"formToken": "' . $formToken . '"", "_type": "DemoFormToken" }';
